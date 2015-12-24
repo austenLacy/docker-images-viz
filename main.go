bbd583788734cdada39d28cb5c337b1170e9cb86
Austen Lacy <austenlacy@gmail.com>
2015-12-24 11:29:04 -0500

    added some layer info

--- main.go
+++ main.go
@@ -6,18 +6,38 @@ import (
 )
 
 func main() {
-    // endpoint := "unix:///var/run/docker.sock"
-
     // use docker-machine
     client, _ := docker.NewClientFromEnv()
     imgs, _ := client.ListImages(docker.ListImagesOptions{All: false})
+
+	if len(imgs) == 0 {
+		fmt.Println("No images available")
+	}
+
     for _, img := range imgs {
-        fmt.Println("ID: ", img.ID)
-        fmt.Println("RepoTags: ", img.RepoTags)
-        fmt.Println("Created: ", img.Created)
-        fmt.Println("Size: ", img.Size)
-        fmt.Println("VirtualSize: ", img.VirtualSize)
-        fmt.Println("ParentId: ", img.ParentID)
+		fmt.Println("Image ID: ", img.ID)
+		fmt.Println("Image RepoTags: ", img.RepoTags)
+		fmt.Println("Image Created: ", img.Created)
+        fmt.Println("Image Size: ", img.Size)
+
+		// IMAGE HISTORY INFO
+		//
+		// Shows each of the layers that make up this image
+		//
+		// for more granular info on the image see -->
+		//     client.InspectImage(img.ID)
+		//
+		imgHistory, _ := client.ImageHistory(img.ID)
+
+		for _, imgHist := range imgHistory {
+			fmt.Println("Img History ID: ", imgHist.ID)
+			fmt.Println("Img History Tags: ", imgHist.Tags)
+			fmt.Println("Img History Created: ", imgHist.Created)
+			fmt.Println("Img History Created By: ", imgHist.CreatedBy)
+			fmt.Println("Img History Size: ", imgHist.Size)
+			fmt.Println("")
+		}
+		fmt.Println("===============================================")
         fmt.Println("")
     }
 }
